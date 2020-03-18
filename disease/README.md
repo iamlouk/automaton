071c258e1dd2399fa5442ca508cf3b5442f868e8
Lou Knauer <lou.knauer98@gmail.com>
2020-03-18 16:17:15 +0100

    new disease simulation

--- disease/README.md
+++ disease/README.md
@@ -1,3 +1,4 @@
+This is not really a cellular automata, but its cool and started as one.
 
 - Build: `wasm-pack build --target web`
 
--- disease/src/lib.rs
+++ disease/src/lib.rs
@@ -3,222 +3,212 @@ use std::{u32, f64};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::{JsCast, JsValue};
 
-const INFECTION_ON_CONTACT_RATE: f64 = 0.1;
-const DEATH_RATE: f64 = 0.005;
-const CURED_RATE: f64 = 0.05;
-const CELL_WIDTH: f64 = 10.;
-const CELL_HEIGHT: f64 = 10.;
+const INDIVIDUAL_RADIUS: f64 = 7.5;
+const TIME_TO_HEAL: i32 = 500;
+const POPULATION_DENSITY: f64 = 15.0;
+const SPEED: f64 = 2.5;
 
 #[wasm_bindgen]
-#[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Cell {
+pub enum State {
     Healthy,
     Infected,
     Cured,
-    Dead,
-    None
+    Dead
 }
 
 #[wasm_bindgen]
-pub struct Universe {
-    width: i32,
-    height: i32,
-    cells: Vec<Cell>,
-    new_cells: Vec<Cell>,
+#[derive(Clone, Debug, PartialEq)]
+pub struct Individual {
+    x: f64,
+    y: f64,
+    movement_x: f64,
+    movement_y: f64,
+    state: State,
+    infected_for: i32
+}
+
+impl Individual {
+    fn update_position(&mut self, width: f64, height: f64) {
+        self.x += self.movement_x;
+        self.y += self.movement_y;
+
+        if self.x < INDIVIDUAL_RADIUS || self.x + INDIVIDUAL_RADIUS >= width {
+            self.movement_x *= -1.;
+            self.x += self.movement_x;
+        }
+
+        if self.y < INDIVIDUAL_RADIUS || self.y + INDIVIDUAL_RADIUS >= height {
+            self.movement_y *= -1.;
+            self.y += self.movement_y;
+        }
+    }
+
+    fn toches(&self, other: &Individual) -> bool {
+        let d_x = self.x - other.x;
+        let d_y = self.y - other.y;
+        let distance = (d_x*d_x + d_y*d_y).sqrt();
+
+        distance < INDIVIDUAL_RADIUS * 2.
+    }
+}
+
+#[wasm_bindgen]
+pub struct Simulation {
+    width: f64,
+    height: f64,
+    individuals: Vec<Individual>,
     ctx: web_sys::CanvasRenderingContext2d,
+
+    color_healthy: JsValue,
+    color_infected: JsValue,
+    color_cured: JsValue,
+    color_dead: JsValue,
+    color_text: JsValue
+}
+
+fn random_normalized_vec() -> (f64, f64) {
+    let x: f64 = js_sys::Math::random() * 2.0 - 1.0;
+    let y: f64 = js_sys::Math::random() * 2.0 - 1.0;
+
+    let len = (x*x + y*y).sqrt() * (1.0 / SPEED);
+
+    (x / len, y / len)
 }
 
 #[wasm_bindgen]
-impl Universe {
+impl Simulation {
+    pub fn new(width: f64, height: f64) -> Self {
+        let document = web_sys::window().unwrap().document().unwrap();
+        let canvas = document.get_element_by_id("canvas").unwrap();
+        let canvas: web_sys::HtmlCanvasElement = canvas
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .map_err(|_| ())
+            .unwrap();
 
-    fn new(width: i32, height: i32, ctx: web_sys::CanvasRenderingContext2d) -> Self {
-        let size = (width * height) as usize;
-        let mut universe = Universe {
+        let ctx = canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .unwrap();
+        
+        ctx.set_font("20px monospace");
+
+        let rows = (height / INDIVIDUAL_RADIUS / POPULATION_DENSITY) as usize;
+        let cols = (width / INDIVIDUAL_RADIUS / POPULATION_DENSITY) as usize;
+        let mut individuals = Vec::with_capacity(rows * cols);
+        for row in 0..rows {
+            for col in 0..cols {
+                let (movement_x, movement_y) = random_normalized_vec();
+
+                let state = if row == rows / 2 && col == cols / 2 {
+                    State::Infected
+                } else {
+                    State::Healthy
+                };
+
+                let x = 50.0 + (width - 100.0) * (row as f64 / rows as f64);
+                let y = 50.0 + (height - 100.0) * (col as f64 / cols as f64);
+
+                individuals.push(Individual {
+                    x, y,
+                    movement_x,
+                    movement_y,
+                    state,
+                    infected_for: 0
+                })
+            }
+        }
+
+        Simulation {
             width,
             height,
-            cells: vec![Cell::Healthy; size],
-            new_cells: vec![Cell::None; size],
-            ctx
-        };
+            individuals,
+            ctx,
 
-        let idx = universe.get_index(width / 2, height / 2);
-        universe.cells[idx] = Cell::Infected;
-
-        universe
+            color_cured: JsValue::from_str("#00ff00"),
+            color_dead: JsValue::from_str("#000000"),
+            color_healthy: JsValue::from_str("#0099ff"),
+            color_infected: JsValue::from_str("#ff3300"),
+            color_text: JsValue::from_str("#555555")
+        }
     }
 
-    /*
-    fn random_pos(&self) -> (i32, i32) {
-        let a = js_sys::Math::random();
-        let b = js_sys::Math::random();
-        ((a * self.height as f64) as i32, (b * self.width as f64)  as i32)
-    }
-    */
+    pub fn step(&mut self) -> Vec<i32> {
+        let mut total_healthy = 0;
+        let mut total_infected = 0;
+        let mut total_cured = 0;
+        let mut total_dead = 0;
 
-    fn get_index(&self, row: i32, col: i32) -> usize {
-        (row * self.width + col) as usize
-    }
-
-    fn get_neighbour_cells(&self, row: i32, col: i32) -> [Cell; 8] {
-        let wrappos = |pos, offset, max| match (pos, offset) {
-            (0,  -1)                   => max - 1,
-            (pos, 1) if pos == max - 1 => 0,
-            (pos, offset)              => pos + offset
-        };
-
-        let getcell = |di, dj| self.cells[self.get_index(wrappos(row, di, self.height), wrappos(col, dj, self.width))];
-
-        [
-            getcell(-1, -1), getcell(-1,  0), getcell(-1,  1),
-            getcell( 0, -1),                  getcell( 0,  1),
-            getcell( 1, -1), getcell( 1,  0), getcell( 1,  1)
-        ]
-    }
-
-    fn get_infected_neighbours(&self, row: i32, col: i32) -> usize {
-        let mut infected = 0;
-
-        for cell in self.get_neighbour_cells(row, col).iter() {
-            if cell == &Cell::Infected {
-                infected += 1;
+        for individual in &mut self.individuals {
+            individual.update_position(self.width, self.height);
+            if individual.state == State::Infected {
+                individual.infected_for += 1;
+                if individual.infected_for == TIME_TO_HEAL {
+                    individual.state = State::Cured;
+                }
             }
         }
 
-        infected
-    }
+        for i in 0..self.individuals.len() {
+            for j in 0..i {
+                if self.individuals[i].toches(&self.individuals[j]) {
+                    let state_a = self.individuals[i].state;
+                    let state_b = self.individuals[j].state;
 
-    pub fn update(&mut self) {
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let r = js_sys::Math::random();
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-
-                if cell == Cell::Healthy {
-                    let infected_neighbours = self.get_infected_neighbours(row, col);
-                    if r <= infected_neighbours as f64 * INFECTION_ON_CONTACT_RATE {
-                        self.new_cells[idx] = Cell::Infected;
-                        continue;
-                    }
-                }
-
-                if cell == Cell::Infected {
-                    if r <= DEATH_RATE {
-                        self.new_cells[idx] = Cell::Dead;
-                        continue;
+                    if (state_a == State::Infected || state_b == State::Infected) && !(state_a == State::Cured || state_b == State::Cured) {
+                        self.individuals[i].state = State::Infected;
+                        self.individuals[j].state = State::Infected;
                     }
 
-                    if r <= CURED_RATE {
-                        self.new_cells[idx] = Cell::Cured;
-                        continue;
-                    }
-                }
+                    let tmp_x = self.individuals[i].movement_x;
+                    let tmp_y = self.individuals[i].movement_y;
+                    self.individuals[i].movement_x = self.individuals[j].movement_x;
+                    self.individuals[i].movement_y = self.individuals[j].movement_y;
+                    self.individuals[j].movement_x = tmp_x;
+                    self.individuals[j].movement_y = tmp_y;
 
-                /*
-                if r >= 0.95 && cell == Cell::Infected {
-                    let r = (r * 10000) as u64;
-                    let (drow, dcol) = match r & 0b11 {
-                        0b00 => ( 0,  1),
-                        0b01 => ( 0, -1),
-                        0b10 => ( 1,  0),
-                        0b11 => (-1,  0),
-                        _ => panic!()
-                    };
-
-                    let npos = match (row + drow, col + dcol) {
-                        (
-                    };
-                }
-                */
-
-                self.new_cells[idx] = cell;
-            }
-        }
-
-        std::mem::swap(&mut self.cells, &mut self.new_cells);
-
-        /*
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let r = js_sys::Math::random();
-                if r < 0.05 {
-                    let (nrow, ncol) = self.random_pos();
-                    let idx1 = self.get_index(row, col);
-                    let idx2 = self.get_index(nrow, ncol);
-
-                    let tmp = self.cells[idx1];
-                    self.cells[idx1] = self.cells[idx2];
-                    self.cells[idx2] = tmp;
-                }
-            }
-        }
-        */
-    }
-
-    pub fn tick(&mut self) -> Vec<i32> {
-        let color_cured    = JsValue::from_str("#00ff00");
-        let color_dead     = JsValue::from_str("#000000");
-        let color_healthy  = JsValue::from_str("#0099ff");
-        let color_infected = JsValue::from_str("#ff3300");
-
-        let mut healthy = 0;
-        let mut infected = 0;
-        let mut cured = 0;
-        let mut dead = 0;
-
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                if self.new_cells[idx] != cell {
-                    self.ctx.set_fill_style(match cell {
-                        Cell::Cured    => &color_cured,
-                        Cell::Dead     => &color_dead,
-                        Cell::Healthy  => &color_healthy,
-                        Cell::Infected => &color_infected,
-                        Cell::None     => panic!()
-                    });
-
-                    self.ctx.fill_rect(col as f64 * CELL_WIDTH, row as f64 * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
-                }
-
-                match cell {
-                    Cell::Cured    => cured += 1,
-                    Cell::Dead     => dead += 1,
-                    Cell::Healthy  => healthy += 1,
-                    Cell::Infected => infected += 1,
-                    Cell::None     => panic!()
+                    self.individuals[i].update_position(self.width, self.height);
+                    self.individuals[j].update_position(self.width, self.height);
                 }
             }
         }
 
-        vec![healthy, infected, cured, dead]
+        self.ctx.clear_rect(0., 0., self.width, self.height);
+
+        for individual in &self.individuals {
+            self.ctx.set_fill_style(match individual.state {
+                State::Healthy => {
+                    total_healthy += 1;
+                    &self.color_healthy
+                },
+                State::Infected => {
+                    total_infected += 1;
+                    &self.color_infected
+                },
+                State::Cured => {
+                    total_cured += 1;
+                    &self.color_cured
+                },
+                State::Dead => {
+                    total_dead += 1;
+                    &self.color_dead
+                }
+            });
+
+            self.ctx.begin_path();
+            self.ctx.arc(individual.x, individual.y, INDIVIDUAL_RADIUS, 0.0, 2.0 * f64::consts::PI).unwrap();
+            self.ctx.fill();
+        }
+
+        let text_x = 10.;
+        let text_y = self.height - 30.0;
+        let text = format!("#healthy: {}, #infected: {}, #cured: {}", total_healthy, total_infected, total_cured);
+        self.ctx.set_fill_style(&self.color_text);
+        self.ctx.fill_text(text.as_ref(), text_x, text_y).unwrap();
+
+        vec![total_healthy, total_infected, total_cured, total_dead]
     }
 
 }
-
-#[wasm_bindgen]
-pub fn get_universe() -> Universe {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap();
-
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
-
-    let universe = Universe::new(75, 75, context);
-
-    universe
-}
-
-#[wasm_bindgen(start)]
-pub fn start() {}
-
